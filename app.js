71ba1461fd2c173020675f3c5b684a89117f1896
ugbanger7 <110037388+ugbanger7@users.noreply.github.com>
2022-08-02 07:10:43 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+//var firstName = "Ugo";
+//var lastName = "ogbonna";
+//var age = 30
+
+//var firstName = prompt("What is your first name?");
+//var lastName = prompt("What is your last name?");
+// var noOfDays = prompt("How many days have you stayed?");
+//var noOfHouses = prompt("How many houses do you have?");
+//var pricePerDay = prompt("How much do you pay per day?");
+//var bill = (noOfDays*pricePerDay);
+
+//var message = "Welcome to this class " + firstName + " " + lastName + ", your age is " + age + " years and you have lived for this number of hours: " + hoursLived;
+
+//var message = `Welcome to HMG apartments you have ${noOfHouses} houses, you pay ${pricePerDay} per day, you have stayed ${noOfDays} with us and your bill is ${bill}`;
+//console.log(message);
+//alert(message
+
+// var firstNumber = prompt("What is the first number?")
+// var lastNumber = prompt("What is the last number?")
+
+var 
